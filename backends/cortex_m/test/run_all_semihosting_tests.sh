ed6f2ad8b2361b6e36e512795cf5546a0209530a
Fabien Klein <fabien.klein@arm.com>
2026-09-10 22:27:59 +0200

    [cortex_m] Add license to float test runner

--- backends/cortex_m/test/run_all_semihosting_tests.sh
+++ backends/cortex_m/test/run_all_semihosting_tests.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+# Copyright 2026 Arm Limited and/or its affiliates.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 set -euo pipefail
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../../.. && pwd)"
